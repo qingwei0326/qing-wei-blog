53a88c5f01bbe6d14a2025a051c009a2eada808e
qingwei0326 <qingwei0326@users.noreply.github.com>
2026-06-13 00:21:44 +0800

    chore: add ESLint and Prettier configuration

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import eslintPlugin from '@typescript-eslint/eslint-plugin'
+import tseslint from '@typescript-eslint/parser'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+
+export default [
+  {
+    ignores: ['dist/', 'node_modules/', 'src-tauri/', 'docs/.vitepress/cache/'],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser: tseslint,
+    },
+    plugins: {
+      '@typescript-eslint': eslintPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      ...eslintPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react-hooks/set-state-in-effect': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+]
